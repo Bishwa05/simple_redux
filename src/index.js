dbbe044c4ac2f5b2d5447aa3fbd63806c716f374
Bishwa <bishwa.nayak@sap.com>
2020-03-20 17:11:35 +0530

    simple redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//STORE -> GLOBALIZED STATE
+
+//ACTION -> increment
+
+//REDUCERS ->
+
+
+
+let store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//Display the store
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+
+
+ReactDOM.render(
+<Provider store = {store}>
+<App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
